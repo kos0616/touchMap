f81ee2640beec76700553a68a5ae09c4848089a3
mark <javert0616@gmail.com>
2021-08-04 15:10:04 +0800

    fix path

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/touchMap/' : '/',
   outputDir: 'docs',
   devServer: {
     overlay: {
